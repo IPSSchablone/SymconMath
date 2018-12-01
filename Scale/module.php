942fca32c688ea226f2aec434e9cb534d7faed6b
IPSSchablone <martin.heinzel@t-online.de>
2018-12-01 11:58:10 +0100

    gxcvbsdbv
    sfgbsfgbsf

---
+++ Scale/module.php
@@ -0,0 +1,27 @@
+<?
+    // Klassendefinition
+    class Scale extends IPSModule {
+
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+			
+			$this->RegisterPropertyInteger("SourceID", 0);
+	        $this->RegisterVariableFloat("Value", "Value", "~Temperature");
+
+        }
+
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            echo $this->InstanceID;
+			echo "Hallo Martin";
+        }
+    }
+?>
