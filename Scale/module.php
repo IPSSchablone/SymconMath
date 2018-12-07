0bb9bd6f3d06f2e63a32d91fed446100dc56ee8d
IPSSchablone <martin.heinzel@t-online.de>
2018-12-07 06:53:52 +0100

    First version
    First version what runs under IPS.

--- Scale/module.php
+++ Scale/module.php
@@ -1,26 +1,39 @@
 <?
-    // Klassendefinition
+	/** Scale
+	* @author Martin Heinzel
+	* @date 07.12.2018
+	* @version 0.0.1
+	*
+	* Description: 
+    * This function scales a value according to the form:
+	*
+    *       (( InValue - InLL )                      ) 
+    * Out = (------------------- x ( OutUL - OutLL ) ) + OutLL 
+    *       ( ( InUL - InLL )                        ) 
+	*
+	*
+	* Changing
+	* ----------
+	* @date tt.mm.jjjj
+	* @version x.y.z
+	*
+	* Beschreibung:
+	*
+	*/
+	
     class Scale extends IPSModule {
 
-        // Überschreibt die interne IPS_Create($id) Funktion
-        public function Create() {
-            // Diese Zeile nicht löschen.
-            parent::Create();
-			
-			$this->RegisterPropertyInteger("SourceID", 0);
-	        $this->RegisterVariableFloat("Value", "Value", "~Temperature");
+		/** Scaling
+		* @param[in] InValue Input value (to be scaled)
+		* @param[in] InLL Input lower limit
+		* @param[in] InUL Input upper limit
+		* @param[in] OutLL Output lower limit
+		* @param[in] OutUL Output upper limit
+		* @return ready scaled value
+		*/
 
-        }
-
-        /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-        *
-        * ABC_MeineErsteEigeneFunktion($id);
-        *
-        */
-        public function MeineErsteEigeneFunktion(float $InValue, float $InLL, float $InUL, float $OutLL, float $OutUL) {
-            echo $InValue;
+        public function Scaling(float $InValue, float $InLL, float $InUL, float $OutLL, float $OutUL) {
+            return ($InValue - $InLL) / ($InUL - $InLL) * ($OutUL - $OutLL) + $OutLL; 
         }
     }
 ?>
